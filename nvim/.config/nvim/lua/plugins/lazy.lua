e7f632b552e27575b8b4bfc888bdaf76e4a9dfc4
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2024-06-01 07:52:29 +0300

    update lazy

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -7,7 +7,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.terraform" },
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.java" },
 }
