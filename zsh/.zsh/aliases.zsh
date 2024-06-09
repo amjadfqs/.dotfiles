51f216c92e64d25773f22b269bdd1ce72e25ad1f
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2024-06-09 16:08:40 +0300

    new changes

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,25 @@
+
+# Aliases
+alias vm="nvim"
+alias c="clear"
+alias q="exit"
+alias e="nvim -n ~/ ~/.zsh/.zshrc ~/.aliases ~/.colors ~/.hooks"
+alias r="source ~/.zsh/.zshrc"
+alias h="history -10"
+alias hc="history -c"
+alias hg="history | grep"
+alias lg="lazygit"
+alias ld="lazydocker"
+
+# Netwokr Manager Aliases
+alias lnet='nmcli device wifi list'
+alias con='nmcli device wifi connect'
+alias discon='nmcli device disconnect'
+
+# lsd
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+
