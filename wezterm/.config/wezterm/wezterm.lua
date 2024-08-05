4c456fac57ef87f2b5f350e3a083fe2357d58068
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2024-08-05 11:54:01 +0300

    static colors for wezterm instead of importing them
    for faster startup time

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,32 +1,60 @@
 local wezterm = require("wezterm")
-
 -- [[ constants ]]
 -- padding
 local padding_on = { left = 12, right = 12, top = 8, bottom = 8 }
 local padding_off = { left = 0, right = 0, top = 0, bottom = 0 }
 
--- font
-local font = wezterm.font_with_fallback({
-	{ family = "JetBrains Mono NF" },
-	{ family = "Symbols Nerd Font", scale = 0.88 },
-})
+--gpu
+local gpus = wezterm.gui.enumerate_gpus()
 
 -- [[ config ]]
 local config = wezterm.config_builder()
 
+-- font
+local font = wezterm.font("JetBrainsMono Nerd Font")
+
 -- general
-config.webgpu_power_preference = "HighPerformance"
+config.animation_fps = 60
+config.max_fps = 60
+config.front_end = "WebGpu"
+config.webgpu_preferred_adapter = gpus[2]
 config.enable_wayland = false
 
 -- font
-config.bidi_enabled = true
-config.font = font
-config.font_size = 14
-config.dpi = 96
+config.font_size = 12
 
 -- theme
 config.term = "wezterm"
-config.color_scheme = "Catppuccin Macchiato"
+
+-- ANSI and Bright colors
+config.color_schemes = {
+	["catppuccin-macchiato"] = {
+		foreground = "#cad3f5",
+		background = "#24273a",
+		cursor_bg = "#f4dbd6",
+		cursor_border = "#f4dbd6",
+		cursor_fg = "#181926",
+		selection_bg = "#5b6078",
+		selection_fg = "#cad3f5",
+		scrollbar_thumb = "#5b6078",
+		split = "#6e738d",
+		ansi = { "#494d64", "#ed8796", "#a6da95", "#eed49f", "#8aadf4", "#f5bde6", "#8bd5ca", "#b8c0e0" },
+		brights = { "#5b6078", "#ed8796", "#a6da95", "#eed49f", "#8aadf4", "#f5bde6", "#8bd5ca", "#a5adcb" },
+		indexed = { [16] = "#f5a97f", [17] = "#f4dbd6" },
+		tab_bar = {
+			background = "#181926",
+			inactive_tab_edge = "#363a4f",
+			active_tab = { bg_color = "#c6a0f6", fg_color = "#181926" },
+			inactive_tab = { bg_color = "#1e2030", fg_color = "#cad3f5" },
+			inactive_tab_hover = { bg_color = "#24273a", fg_color = "#cad3f5" },
+			new_tab = { bg_color = "#363a4f", fg_color = "#cad3f5" },
+			new_tab_hover = { bg_color = "#494d64", fg_color = "#cad3f5" },
+		},
+	},
+}
+
+-- Assign the color scheme to use
+config.color_scheme = "catppuccin-macchiato"
 
 -- tab bar
 config.use_fancy_tab_bar = false
@@ -36,10 +64,10 @@ config.tab_max_width = 24
 
 -- window
 config.enable_scroll_bar = false
-config.window_background_opacity = 0.93
+config.window_background_opacity = 0.90
 config.window_frame = { font = font, font_size = 10.0 }
 config.window_padding = padding_on
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 
 -- [[ events ]]
 
@@ -60,6 +88,7 @@ end)
 
 -- [[ key bindings ]]
 config.keys = {
+	{ key = "K", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
 	-- toggle padding
 	{
 		key = "P",
@@ -148,6 +177,8 @@ config.keys = {
 		mods = "CTRL|SHIFT",
 		action = wezterm.action.SplitHorizontal,
 	},
+
+	{ key = "K", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
 }
 
 return config
